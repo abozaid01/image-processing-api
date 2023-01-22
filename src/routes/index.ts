eddd4735d24109aecc216aff86ae9bd1cd3bc700
Mohamed Abozaid <mohamed.abozaid01@gmail.com>
2023-01-23 01:28:32 +0200

    fix error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,6 @@ import { requestValidator } from "../utils/utils";
 
 const router = express.Router();
 
-//get request for /api endpoint
-router.get("/", (req, res) => {
-    res.send("api");
-});
-
 //use express middleware to get /api/image endpoint and apply requestvalidator function to it
 router.use("/", requestValidator, image);
 
